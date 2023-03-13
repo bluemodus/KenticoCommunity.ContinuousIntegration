4b66e4f5a13311a133beb346aa6b5ff5710f610f
Jeff Schreiner <jschreiner@bluemodus.com>
2023-03-13 12:05:04 -0600

    Add support for separated OM database

--- src/KenticoCommunity.ConnectionStringModule/EnvironmentVariableConnectionStringModule.cs
+++ src/KenticoCommunity.ConnectionStringModule/EnvironmentVariableConnectionStringModule.cs
@@ -21,28 +21,37 @@ namespace KenticoCommunity.ConnectionStringModule
             base.OnPreInit();
 
             var isContinuousIntegration = AppDomain.CurrentDomain.FriendlyName.Equals("ContinuousIntegration.exe", StringComparison.OrdinalIgnoreCase);
+
             if (!isContinuousIntegration)
             {
                 return;
             }
-           
-            var azureConnectionString = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_CMSConnectionString");
-            
-            Console.WriteLine();
+
+            SetupConnectionStringFromEnvironment("SQLAZURECONNSTR_", "CMSConnectionString");
+            SetupConnectionStringFromEnvironment("SQLAZURECONNSTR_", "CMSOMConnectionString");
+        }
+
+        private static void SetupConnectionStringFromEnvironment(string prefix, string connectionStringName)
+        {
+            var envVarName = prefix + connectionStringName;
+
+            var azureConnectionString = Environment.GetEnvironmentVariable(envVarName);
 
             if (string.IsNullOrWhiteSpace(azureConnectionString))
             {
-                Console.WriteLine($"SQLAZURECONNSTR_CMSConnectionString environment variable is not set. Nothing to do.{Environment.NewLine}");
-
+                Console.WriteLine($"{Environment.NewLine}{envVarName} environment variable is not set. Nothing to do.{Environment.NewLine}");
                 return;
             }
-          
-            var connectionStringBuilder = new SqlConnectionStringBuilder(azureConnectionString);
-            connectionStringBuilder.Password = "******";
-            var displayConnectionString = connectionStringBuilder.ToString();
-            Console.WriteLine($"Overriding CMSConnectionString using environment variables{Environment.NewLine}SQLAZURECONNSTR_CMSConnectionString={displayConnectionString}{Environment.NewLine}");
 
-            SettingsHelper.ConnectionStrings.SetConnectionString("CMSConnectionString", azureConnectionString);
+            var displayConnectionString = new SqlConnectionStringBuilder(azureConnectionString)
+            {
+                Password = "******"
+            }
+                .ToString();
+
+            Console.WriteLine($"{Environment.NewLine}Overriding {connectionStringName} using environment variables{Environment.NewLine}{envVarName}={displayConnectionString}{Environment.NewLine}");
+
+            SettingsHelper.ConnectionStrings.SetConnectionString(connectionStringName, azureConnectionString);
         }
     }
 }
